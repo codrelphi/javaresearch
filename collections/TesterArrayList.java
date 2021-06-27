725eaf8126601ece06f2cd4ba28da4f33c98af53
codrelphi <chancerel.codjovi@gmail.com>
2021-06-27 22:53:07 +0100

    ajout de TesterArrayList.java

---
+++ collections/TesterArrayList.java
@@ -0,0 +1,75 @@
+/* objectif: quelques opérations sur ArrayList
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 27.06.2021
+ *
+ */
+
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class TesterArrayList {
+
+
+	public static void main(String [] args) {
+
+		/* ArrayList: optimisé pour la lecture.
+		 * quelques méthodes
+		 * add()
+		 * addAll()
+		 * remove()
+		 * removeAll()
+		 * size()
+		 * contains()
+		 * clear()
+		 */
+
+		ArrayList<String> animaux = new ArrayList<String>(5);
+		animaux.add("lion");
+		animaux.add("chat");
+		animaux.add("perroquet");
+		animaux.add("chien");
+		animaux.add("léopard");
+		/* rien ne nous empêche d'ajouter un autre élément à la lsite vue qu'elle 
+		 * est dynamique
+		 */
+		animaux.add("renard");
+		System.out.println("Animaux : " + animaux);
+		
+		String [] autresAnimauxTab = new String[3];
+		autresAnimauxTab[0] = "souris";
+		autresAnimauxTab[1] = "criquet";
+		autresAnimauxTab[2] = "papillon";
+		
+		ArrayList<String> autresAnimaux = new ArrayList<String>(autresAnimauxTab.length);
+		for (var animal : autresAnimauxTab) {
+			autresAnimaux.add(animal);
+		}
+		animaux.addAll(0, autresAnimaux);
+
+		System.out.println("Animaux : " + animaux);
+
+		Scanner lectureClavier = new Scanner(System.in);
+		int indice;
+		do {
+			System.out.print("Quel indice voulez-vous ? : ");
+			indice = lectureClavier.nextInt();
+		} while(indice < 0 || indice > (animaux.size() - 1));
+		System.out.println(" Vous avez choisi l'animal : " + animaux.get(indice));
+		animaux.remove(indice);
+		System.out.println("Animaux : " + animaux);
+		
+		System.out.print("Est-ce que la liste contient : ");
+		var animalAVerifier = lectureClavier.next();
+		
+		System.out.println("La liste " + animaux);
+		if (animaux.contains(animalAVerifier))
+			System.out.println(" contient : " + animalAVerifier);
+		else 
+			System.out.println(" ne contient pas : " + animalAVerifier);
+		
+	}
+}
+
