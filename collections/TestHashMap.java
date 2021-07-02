660bd31a5876cae2afa79cfb56f51cdb79ba386f
codrelphi <chancerel.codjovi@gmail.com>
2021-07-02 10:28:10 +0100

    ajouter TestHashMap.java

---
+++ collections/TestHashMap.java
@@ -0,0 +1,40 @@
+/* objectif: juste tester HashMap
+ * auteur: Chancerel Codjovi (aka codrelphi)
+ * date: 02.07.2021
+ *
+ */
+
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+public class TestHashMap {
+
+	public static void main(String [] args) {
+
+		HashMap<Integer, String> nombres = new HashMap<Integer, String>();
+		
+		System.out.println("Nombres : " + nombres);
+		System.out.println(" Taille : " + nombres.size());
+		System.out.println(" Vide ou pas ? : " + nombres.isEmpty());
+
+		nombres.put(1, "un");
+		nombres.put(2, "deux");
+		nombres.put(3, "trois");
+		nombres.put(4, "quatre");
+		nombres.put(5, "cinq");
+
+		System.out.println("Nombres : " + nombres);
+		System.out.println(" Taille : " + nombres.size());
+		System.out.println(" Vide ou pas ? : " + nombres.isEmpty());
+
+		Collection c = nombres.values();
+		for (Iterator i = c.iterator(); i.hasNext();) {
+			System.out.println(i.next());
+		
+		}
+
+	}
+
+}
